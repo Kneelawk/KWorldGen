cc8ce561724125ac3cff888890dfb8af9f8dd0de
Kneelawk <jedidiah.pommert@gmail.com>
2016-03-31 21:37:52 -0700

    Finish the table block

--- src/main/java/org/kneelawk/kworldgen/blocks/BlockPhinterineTable.java
+++ src/main/java/org/kneelawk/kworldgen/blocks/BlockPhinterineTable.java
@@ -30,35 +30,7 @@ public class BlockPhinterineTable extends KWGBlock {
 		return false;
 	}
 
-	public boolean isFullyOpaque(IBlockState state) {
-		return false;
-	}
-
 	public boolean isFullBlock(IBlockState state) {
-		return false;
-	}
-
-	public int getLightOpacity(IBlockState state) {
-		return 0;
-	}
-
-	public boolean isBlockNormalCube(IBlockState state) {
-		return false;
-	}
-
-	public boolean isNormalCube(IBlockState state) {
-		return false;
-	}
-
-	public boolean isVisuallyOpaque() {
-		return false;
-	}
-
-	public boolean isBlockSolid(IBlockAccess worldIn, BlockPos pos, EnumFacing side) {
-		return false;
-	}
-
-	public boolean isNormalCube(IBlockState state, IBlockAccess world, BlockPos pos) {
 		return false;
 	}
 }
