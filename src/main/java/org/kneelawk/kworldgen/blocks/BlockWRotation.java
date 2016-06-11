e8f1e74408f6476f23202ec4fcdf34c3d97ddf1d
Kneelawk <jedidiah.pommert@gmail.com>
2016-06-11 02:21:14 -0700

    Finish BlockWRotation class

--- src/main/java/org/kneelawk/kworldgen/blocks/BlockWRotation.java
+++ src/main/java/org/kneelawk/kworldgen/blocks/BlockWRotation.java
@@ -8,13 +8,18 @@ import com.google.common.collect.ImmutableMap;
 import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.PropertyEnum;
+import net.minecraft.block.state.BlockStateContainer;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
-public class BlockWRotation extends KWGBlock implements IBlockWMeta {
+public class BlockWRotation extends KWGBlock {
 
 	public static final String AXIS_FIELD_NAME = "axis";
-	public static final PropertyEnum<EnumFacing.Axis> AXIS = PropertyEnum
-			.create(AXIS_FIELD_NAME, EnumFacing.Axis.class);
+	public static final PropertyEnum<EnumFacing.Axis> AXIS =
+			PropertyEnum.create(AXIS_FIELD_NAME, EnumFacing.Axis.class);
 
 	public BlockWRotation(Material blockMaterialIn, MapColor blockMapColorIn) {
 		super(blockMaterialIn, blockMapColorIn);
@@ -25,13 +30,33 @@ public class BlockWRotation extends KWGBlock implements IBlockWMeta {
 	}
 
 	@Override
-	public Map<String, Integer> getPossibleMetaValues() {
-		ImmutableMap.Builder<String, Integer> builder = ImmutableMap.builder();
-		for (EnumFacing.Axis axis : EnumFacing.Axis.values()) {
-			builder.put(AXIS_FIELD_NAME + "=" + axis.getName(), axis.ordinal());
-		}
-		return builder.build();
+	public BlockStateContainer getBlockState() {
+		return new BlockStateContainer(this, AXIS);
 	}
 
-	// TODO more on BlockWRotation class
+	@Override
+	public IBlockState getStateFromMeta(int meta) {
+		return getDefaultState().withProperty(AXIS, getAxisFromMeta(meta));
+	}
+
+	@Override
+	public int getMetaFromState(IBlockState state) {
+		return getMetaFromAxis(state.getValue(AXIS));
+	}
+
+	@Override
+	public IBlockState onBlockPlaced(World worldIn, BlockPos pos,
+			EnumFacing facing, float hitX, float hitY, float hitZ, int meta,
+			EntityLivingBase placer) {
+		return getDefaultState().withProperty(AXIS, facing.getAxis());
+	}
+
+	public static EnumFacing.Axis getAxisFromMeta(int meta) {
+		EnumFacing.Axis[] values = EnumFacing.Axis.values();
+		return values[meta % values.length];
+	}
+
+	public static int getMetaFromAxis(EnumFacing.Axis axis) {
+		return axis.ordinal();
+	}
 }
